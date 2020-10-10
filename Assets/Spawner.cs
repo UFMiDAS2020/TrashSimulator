e73c0ed4734b250734a9ec90214183f835667ff1
Smith <linkraft@ufl.edu>
2020-10-10 01:40:32 -0400

    Added some VR stuff

---
+++ Assets/Spawner.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public int numberOfObjectsToSpawn;
+    public Rigidbody objectToSpawn;
+
+    public float timeOffset = 2f;
+    private float timer;
+
+    private void Start()
+    {
+        timer = timeOffset;
+    }
+
+    private void Update()
+    {
+        if (timer <= 0f)
+        {
+            if (numberOfObjectsToSpawn > 0)
+            {
+                Rigidbody p = Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+                p.velocity = transform.forward * 10;
+            }
+            numberOfObjectsToSpawn--;
+            timer = timeOffset;
+        }
+        timer -= Time.deltaTime;
+    }
+}
