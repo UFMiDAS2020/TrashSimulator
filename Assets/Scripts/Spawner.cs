005556ef31e6c77f83b7a35210ba52c4a37398d8
Smith <linkraft@ufl.edu>
2020-10-11 02:05:56 -0400

    Free play is a go

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -2,15 +2,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Spawner : MonoBehaviour
-{
-    public int numberOfObjectsToSpawn;
+public class Spawner : MonoBehaviour {
+    public ToggleGroup toggleGroup;
+    public Slider objectSlider;
     public Rigidbody objectToSpawn;
-
     public float timeOffset = 2f;
+
+    private int numberOfObjectsToSpawn;
     private float timer;
     private bool spawning;
 
@@ -23,11 +26,12 @@ public class Spawner : MonoBehaviour
     {
         if (spawning)
         {
+            
             if (timer <= 0f)
             {
                 if (numberOfObjectsToSpawn > 0)
                 {
-                    Rigidbody p = Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+                    Rigidbody p = Instantiate(objectToSpawn, transform.position, UnityEngine.Random.rotation);
                     p.velocity = transform.forward * 10;
                 }
                 numberOfObjectsToSpawn--;
@@ -38,13 +42,21 @@ public class Spawner : MonoBehaviour
         }
     }
 
-    public void SpawnObjects(int numObjects)
+    public void SpawnObjects()
     {
         if (!spawning)
         {
+            int multiplier = GetMultiplier();
+            numberOfObjectsToSpawn = (int)Math.Floor(objectSlider.value) * multiplier;
             spawning = true;
-            numberOfObjectsToSpawn = numObjects;
         }
     }
 
+    public int GetMultiplier()
+    {
+        Toggle toggle = toggleGroup.ActiveToggles().FirstOrDefault();
+        if (toggle.name == "Yearly Toggle") return 1;
+        else if (toggle.name == "Monthly Toggle") return 12;
+        else return 52;
+    }
 }
